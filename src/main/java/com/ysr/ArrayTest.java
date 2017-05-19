539c52d711cbc1a85cff28191b64519cc1f035fc
ysrfeng <ysr200808@163.com>
2017-05-19 17:45:28 +0800

    集合

---
+++ src/main/java/com/ysr/ArrayTest.java
@@ -0,0 +1,14 @@
+package com.ysr;
+
+/**
+ * Created by Administrator on 2017/5/19.
+ */
+public class ArrayTest {
+    public static void main(String... args) {
+        for (String ar : args
+                ) {
+            System.out.println(ar);
+        }
+
+    }
+}
---
+++ src/main/kotlin/com/ysr/moudle/AggregateInfo.kt
@@ -0,0 +1,17 @@
+package com.ysr.moudle
+
+/**集合
+ * Created by Administrator on 2017/5/19.
+ */
+//class AggregateInfo
+fun main(array: Array<out String>) {
+    array.map(::println)
+//    array.map {
+//       println(it)
+//    }
+
+//    for (ar in array) {
+//        println(ar)
+//    }
+
+}
