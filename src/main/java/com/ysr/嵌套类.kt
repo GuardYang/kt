fb25fb41c4c950026eaa89517c6b32241f84334b
ysrfeng <ysr200808@163.com>
2017-06-21 20:47:44 +0800

    commit

---
+++ src/main/java/com/ysr/嵌套类.kt
@@ -0,0 +1,30 @@
+package com.ysr
+
+/**
+ * Created by Administrator on 2017/6/21.
+ */
+class News {
+    private var address = "dawdaf"
+
+    class Cate {
+        var list = arrayOf("Java", "Swift", "C++", "Kotlin")
+        //joinToString输出文本
+        val listDesc = list.joinToString()
+
+    }
+
+    //内部类
+    inner class Langgue {
+        fun getDate(name: String) {
+            //内部类可以访问主类属性
+            address = name
+        }
+    }
+}
+
+fun main(array: Array<String>) {
+//    println(News.Cate().listDesc)
+    News.Cate().listDesc.let (::println)
+    //更改地区
+
+}
